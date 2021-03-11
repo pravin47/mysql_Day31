e5a69db152be2dc82a8ea0b9859a0d0b34a0a0af
pravin bagul <bagul.pravin2@gmail.com>
2021-03-11 14:46:56 +0530

    Address_Book_Service

--- sql.java
+++ sql.java
@@ -1,64 +1,60 @@
-Employee Payroll Service
+Address Book Service  
 
 UC1:
-> Create database employee_payroll;
-> use employee_payroll;
+> create database AddressBook;
+> use AddressBook;
 
 
 UC2:
-> create table Employee_Payroll_Table (Id Integer auto_increment primary key,Name varchar(30),Salary double,Start_Date DATE);
+> create table Address_Book_Table (First_Name varchar(10),Last_Name varchar(10),Address varchar(100),City varchar(15),State varchar   (20),Zip int(10),Phone_Number int(15),Email varchar(20));
 
 
 UC3:
-> insert into Employee_Payroll_Table values(1'pravin',30000,'1994-01-05');  
-> insert into Employee_Payroll_Table values(2'ravi',30000,'1994-01-05');
-> insert into Employee_Payroll_Table values(3,'avinash',40000,'1998-01-05');  
-> insert into Employee_Payroll_Table values(4,'raju',37000,'1997-01-05');
-> insert into Employee_Payroll_Table values(5,'ajay',38000,'1998-01-05');
-
+> select * from Address_Book_Table;
+> insert into Address_Book_Table value('pravin','bagul','ram nagar sakri','sakri','maharashtra','424304','9767','bagul2@gmail.com');
+> insert into Address_Book_Table value('rahul','deshmukh','nagai colony dhule','dhule','gujrat',586954,857455,'rahul3@gmail.com');
+> insert into Address_Book_Table value('anil','patil','rupai nagar mumbai','mumbai','odisha',69857,85652,'patil4@gmail.com');
 
 UC4:
-> select * from Employee_Payroll_Table;
-
+> update Address_Book_Table set City ='aurangabad' where first_Name = 'pravin';
 
 UC5:
-> select salary from Employee_Payroll_Table where Name = 'ravi';
-> select * from  Employee_Payroll_Table WHERE Start_Date BETWEEN CAST('1996-01-01'AS DATE) AND DATE(NOW());
-
+> delete from Address_Book_Table where First_Name ='anil';
 
 UC6:
-> alter table Employee_Payroll_Table ADD COLUMN Gender Varchar(10) AFTER Name; 
-
+> select * from Address_Book_Table where City = 'dhule' or State = 'karnatak'; 
 
 UC7:
-> select sum(Salary) from Employee_Payroll_Table where Gender = 'male' GROUP BY Gender;
-> select avg(Salary) from Employee_Payroll_Table where Gender = 'male' GROUP BY Gender;
-> select min(Salary) from Employee_Payroll_Table where Gender = 'male' GROUP BY Gender;    
-> select max(Salary) from Employee_Payroll_Table where Gender = 'male' GROUP BY Gender;
-> select count(Salary) from Employee_Payroll_Table where Gender = 'male' GROUP BY Gender;
+> select COUNT(*) from Address_Book_Table where City ='nashik' and State = 'karnatak';
 
-
-UC8:
-> alter table Employee_Payroll_Table ADD COLUMN Department varchar(30) NOT NULL;
-> alter table Employee_Payroll_Table ADD COLUMN Address varchar(100) DEFAULT 'null';
-> alter table Employee_Payroll_Table ADD COLUMN Phone_Number Integer (12) ;
+UC8: 
+> select * from Address_Book_Table  WHERE City = 'dhule' ORDER BY First_Name ASC; 
 
 
 UC9:
-> alter table employee_payroll_table rename column Salary to Basic_Pay;
-> alter table employee_payroll_table add column Deductions integer(50) after Basic_Pay;
-> alter table employee_payroll_table add column Taxable_Pay integer(50) after Deductions;
-> alter table employee_payroll_table add column Income_Tax integer(50) after Taxable_Pay;
-> alter table employee_payroll_table add column Net_Pay integer(50) after Income_Tax;
- 
+> alter table Address_Book_Table ADD COLUMN Type varchar(15) AFTER Email;
+> update Address_book_table set type ='family'where First_Name = 'pravin';
+> update Address_book_table set type ='family'where First_Name = 'rahul';
+> update Address_book_table set type ='friend'where First_Name = 'anil';
+> update Address_book_table set type ='family'where First_Name = 'sunil';
+> update Address_book_table set type ='friend'where First_Name = 'sumit';
 
-uc10:
 
-> create table Department(Dep_Id integer(10)primary key,Dep_Name varchar(50),Employee_Id integer(10) ,foreign key(Employee_Id)   references employee_payroll_table(Id);
-> insert into Department values(1,'sales',11); 
-> insert into Department values(2,'marketing',11); 
-> insert into Department values(3,'hr',5); 
-> selectemployee_payroll_table.Name,employee_payroll_table.Gender,employee_payroll_table.Basic_Pay,department.Dep_Name,department.Dep_Idfrom department inner join employee_payroll_table on department.Employee_Id = employee_payroll_table.Id;
+
+UC10:
+> select count(*) from Address_Book_Table where Type ='family';
+
+
+UC11:
+> update Address_book_table set Type = 'Family/friend' where First_Name ='pravin'; 
+
+
+
+
+
+
+
+
 
 
 
